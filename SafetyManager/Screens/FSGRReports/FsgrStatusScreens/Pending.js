c1dc6fb9d0fd9e6090bee4a83672ad383687f68b
Fahad Mahmood <fahadmahmood@Fahads-MacBook-Air.local>
2024-06-10 20:41:36 +0530

    commit

--- SafetyManager/Screens/FSGRReports/FsgrStatusScreens/Pending.js
+++ SafetyManager/Screens/FSGRReports/FsgrStatusScreens/Pending.js
@@ -10,14 +10,13 @@ import React, { useEffect, useState } from "react";
 import { serveraddress } from "../../../../assets/values/Constants";
 import axios from "axios";
 import BottomPopup from "./BottomPopup";
-import { AntDesign, Entypo, SimpleLineIcons } from "@expo/vector-icons";
+import { SimpleLineIcons } from "@expo/vector-icons";
 
 const Pending = ({ loadSearchBar }) => {
   const [isVisible, setIsVisible] = useState(false);
   const [id, setId] = useState(0);
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [dataNotFound, setDataNotFound] = useState(false);
   const [searchLocation, setSearchLocation] = useState("");
 
   useEffect(() => {
@@ -25,23 +24,15 @@ const Pending = ({ loadSearchBar }) => {
   }, [searchLocation]);
 
   const fetchData = async () => {
+    setLoading(true);
     try {
-      setLoading(true);
-      const response = await axios.get(`${serveraddress}fsgr/form/pending/${searchLocation}`
-      //   , {
-      //   params: {
-      //     location: searchLocation, // Pass location as a query parameter
-      //   },
-      // }
-    );
-      if (response.data) {
-        setData(response.data);
-        console.log(response.data);
-      } else {
-        setDataNotFound(true);
-      }
+      const response = await axios.get(
+        `${serveraddress}fsgr/form/pending/${searchLocation}`
+      );
+      setData(response.data || []);
     } catch (error) {
       console.error("Error fetching data:", error.message);
+      setData([]);
     } finally {
       setLoading(false);
     }
@@ -49,156 +40,66 @@ const Pending = ({ loadSearchBar }) => {
 
   return (
     <View style={styles.mainContainer}>
-      {loadSearchBar ? (
-        <View
-          style={{
-            flex: 1,
-            width: "100%",
-            marginBottom: 20,
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-evenly",
-          }}
-        >
+      {loadSearchBar && (
+        <View style={styles.searchBarContainer}>
           <TextInput
-            style={{
-              width: "90%",
-              height: 42,
-              borderWidth: 0.5,
-              // alignSelf: "center",
-              borderRadius: 8,
-              paddingStart: 10,
-            }}
+            style={styles.searchInput}
             placeholder="Search by location"
-            onChangeText={(text) => setSearchLocation(text)}
+            onChangeText={setSearchLocation}
           />
-          <TouchableOpacity
-            style={{
-              display: "flex",
-              width: "10%",
-              position: "absolute",
-              left: 300,
-              marginTop: 8,
-              borderLeftWidth: 0.3,
-              borderLeftColor: "grey",
-              padding: 3,
-              alignSelf: "center",
-            }}
-            onPress={fetchData}
-          >
+          <TouchableOpacity style={styles.searchButton} onPress={fetchData}>
             <SimpleLineIcons
               name="magnifier"
               size={20}
               color="blue"
-              style={{ marginStart: 8 }}
+              style={styles.searchIcon}
             />
           </TouchableOpacity>
         </View>
-      ) : null}
+      )}
       {loading ? (
         <ActivityIndicator size="large" color="#0000ff" />
-      ) : data ? (
-        data.map((item) => {
-          return (
-            <TouchableOpacity
-              style={styles.container}
-              key={item.id}
-              onPress={() => {
-                setIsVisible(true);
-                setId(item.id);
-              }}
-            >
-              <View
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <View style={{ width: "70%" }}>
-                  <Text
-                    style={{
-                      fontSize: 16,
-                      fontWeight: "500",
-                      color: "#505050",
-                    }}
-                  >
-                    {item.heading}
-                  </Text>
-                  <View
-                    style={{
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "flex-start",
-                      alignItems: "start",
-                    }}
-                  >
-                    <View
-                      style={{ flexDirection: "row", alignItems: "center" }}
-                    >
-                      <Text style={styles.text}>Location</Text>
-                      <Text style={styles.textLocation}>
-                        {item.location === null ? "" : item.location}
-                      </Text>
-                    </View>
-
-                    <View
-                      style={{ flexDirection: "row", alignItems: "center" }}
-                    >
-                      <Text
-                        style={{
-                          marginRight: 10,
-                          fontSize: 13,
-                          fontWeight: "300",
-                          color: "black",
-                        }}
-                      >
-                        {item.createdAt.slice(0, 10)}
-                      </Text>
-                      <Text
-                        style={{
-                          fontSize: 12,
-                          fontWeight: "700",
-                          color: "#f44336",
-                        }}
-                      >
-                        {item.priority === null ? "" : item.priority}
-                      </Text>
-                    </View>
+      ) : data.length > 0 ? (
+        data.map((item) => (
+          <TouchableOpacity
+            style={styles.container}
+            key={item.id}
+            onPress={() => {
+              setIsVisible(true);
+              setId(item.id);
+            }}
+          >
+            <View style={styles.itemContainer}>
+              <View style={styles.itemDetails}>
+                <Text style={styles.itemHeading}>{item.heading}</Text>
+                <View style={styles.itemInfo}>
+                  <View style={styles.itemRow}>
+                    <Text style={styles.text}>Location</Text>
+                    <Text style={styles.textLocation}>
+                      {item.location || ""}
+                    </Text>
                   </View>
-                </View>
-                <View style={{ width: "30%" }}>
-                  <View
-                    style={{
-                      backgroundColor: "#fff4e5",
-                      paddingHorizontal: 2,
-                      paddingVertical: 5,
-                      borderRadius: 5,
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }}
-                  >
-                    <Text
-                      style={{
-                        fontSize: 12,
-                        fontWeight: 600,
-                        color: "#663c00",
-                      }}
-                    >
-                      Pending
+                  <View style={styles.itemRow}>
+                    <Text style={styles.itemDate}>
+                      {item.createdAt.slice(0, 10)}
+                    </Text>
+                    <Text style={styles.itemPriority}>
+                      {item.priority || ""}
                     </Text>
                   </View>
                 </View>
               </View>
-            </TouchableOpacity>
-          );
-        })
-      ) : dataNotFound ? (
+              <View style={styles.statusContainer}>
+                <View style={styles.statusBadge}>
+                  <Text style={styles.statusText}>Pending</Text>
+                </View>
+              </View>
+            </View>
+          </TouchableOpacity>
+        ))
+      ) : (
         <Text>No data found</Text>
-      ) : null}
-
+      )}
       <BottomPopup isVisible={isVisible} setIsVisible={setIsVisible} id={id} />
     </View>
   );
@@ -209,6 +110,35 @@ const styles = StyleSheet.create({
     height: "100%",
     justifyContent: "center",
     alignItems: "center",
+  },
+  searchBarContainer: {
+    flex: 1,
+    width: "100%",
+    marginBottom: 20,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  searchInput: {
+    width: "90%",
+    height: 42,
+    borderRadius: 8,
+    paddingStart: 10,
+    backgroundColor: "white",
+    elevation: 5,
+    marginTop: 5,
+  },
+  searchButton: {
+    width: "10%",
+    position: "absolute",
+    left: 300,
+    marginTop: 8,
+    borderLeftWidth: 0.3,
+    borderLeftColor: "grey",
+    padding: 5,
+    alignSelf: "center",
+  },
+  searchIcon: {
+    marginStart: 8,
   },
   container: {
     backgroundColor: "#fffbfe",
@@ -221,8 +151,27 @@ const styles = StyleSheet.create({
     elevation: 5,
     marginBottom: 20,
   },
-  color: {
-    color: "#21005d",
+  itemContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  itemDetails: {
+    width: "70%",
+  },
+  itemHeading: {
+    fontSize: 16,
+    fontWeight: "500",
+    color: "#505050",
+  },
+  itemInfo: {
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "start",
+  },
+  itemRow: {
+    flexDirection: "row",
+    alignItems: "center",
   },
   text: {
     paddingVertical: 4,
@@ -236,6 +185,33 @@ const styles = StyleSheet.create({
     fontWeight: "600",
     color: "#21005d",
     marginLeft: 10,
+  },
+  itemDate: {
+    marginRight: 10,
+    fontSize: 13,
+    fontWeight: "300",
+    color: "black",
+  },
+  itemPriority: {
+    fontSize: 12,
+    fontWeight: "700",
+    color: "#f44336",
+  },
+  statusContainer: {
+    width: "30%",
+  },
+  statusBadge: {
+    backgroundColor: "#fff4e5",
+    paddingHorizontal: 2,
+    paddingVertical: 5,
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  statusText: {
+    fontSize: 12,
+    fontWeight: "600",
+    color: "#663c00",
   },
 });
 
--- assets/values/Constants.js
+++ assets/values/Constants.js
@@ -1,4 +1,4 @@
-const serveraddress = "http://192.168.1.167:8085/";
-// const serveraddress = "https://sms-be-fj4h.onrender.com/";
+// const serveraddress = "http://192.168.1.167:8085/";
+const serveraddress = "https://sms-be-fj4h.onrender.com/";
 
 export { serveraddress };
