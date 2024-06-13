24750965bc4627f92c87dc17a020ec7eff2214c3
Fahad Mahmood <fahadmahmood@Fahads-MacBook-Air.local>
2024-06-13 18:18:00 +0530

    Fsgr search field

--- screens/FsgrInfo/FsgrInfo.js
+++ screens/FsgrInfo/FsgrInfo.js
@@ -1,20 +1,20 @@
-import { useEffect, useState } from "react";
+// FsgrInfo.js
+import React, { useEffect, useState } from "react";
 import {
   View,
   Text,
   StyleSheet,
   TouchableOpacity,
   ActivityIndicator,
+  ScrollView,
 } from "react-native";
-
 import { Appbar } from "react-native-paper";
-import { Dropdown } from "react-native-element-dropdown";
-import { AntDesign } from "@expo/vector-icons";
 import axios from "axios";
 import { useNavigation } from "@react-navigation/native";
 import { serveraddress } from "../../assets/values/Constants";
 import BottomPopup from "../../components/Global/UnderConstruction/BottomPopup";
 import { fetchLocations } from "../../components/Global/Global";
+import SearchForm from "./FsgrSearch";
 
 const FsgrInfo = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -42,45 +42,9 @@ const FsgrInfo = () => {
     fetchLocationsData();
   }, []);
 
-  const handleLocationChange = (item) => {
-    setSelectedLocation(item);
-  };
-
-  const handleMonthChange = (selectedMonth) => {
-    setSelectedMonth(selectedMonth);
-  };
-
-  const handleYearChange = (item) => {
-    setSelectedYear(item);
-  };
-
-  const monthData = [
-    { label: "January", value: "01" },
-    { label: "February", value: "02" },
-    { label: "March", value: "03" },
-    { label: "April", value: "04" },
-    { label: "May", value: "05" },
-    { label: "June", value: "06" },
-    { label: "July", value: "07" },
-    { label: "August", value: "08" },
-    { label: "September", value: "09" },
-    { label: "October", value: "10" },
-    { label: "November", value: "11" },
-    { label: "December", value: "12" },
-  ];
-
-  const yearData = [
-    { label: "2023", value: "2023" },
-    { label: "2024", value: "2024" },
-    { label: "2025", value: "2025" },
-    { label: "2026", value: "2026" },
-    { label: "2027", value: "2027" },
-    { label: "2028", value: "2028" },
-  ];
-
   const searchFsgr = async () => {
     if (!selectedLocation || !selectedMonth || !selectedYear) {
-      console.error("Location and month must be selected");
+      console.error("Location, month, and year must be selected");
       return;
     }
     console.log(
@@ -108,7 +72,12 @@ const FsgrInfo = () => {
   };
 
   return (
-    <View>
+    <View
+      style={{
+        backgroundColor: "white",
+        height: "100%",
+      }}
+    >
       <Appbar.Header>
         <Appbar.BackAction
           onPress={() => {
@@ -123,130 +92,22 @@ const FsgrInfo = () => {
           }}
         />
       </Appbar.Header>
-      <View>
+      <ScrollView>
         <View style={styles.mainContainer}>
           {showSearch && (
-            <View
-              style={{
-                width: "90%",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                // borderWidth: 0.5,
-                borderRadius: 8,
-                backgroundColor: "#ffffff",
-                // shadowColor: "#000",
-                // // Elevation for Android
-                // elevation: 4,
-                marginBottom: 20,
-              }}
-            >
-              <View
-                style={{
-                  marginTop: 20,
-                  marginHorizontal: 20,
-                  width: "100%",
-                }}
-              >
-                <Dropdown
-                  style={styles.dropdown}
-                  placeholderStyle={styles.placeholderStyle}
-                  selectedTextStyle={styles.selectedTextStyle}
-                  inputSearchStyle={styles.inputSearchStyle}
-                  iconStyle={styles.iconStyle}
-                  data={locations.map((location) => ({
-                    label: location.name,
-                    value: location.id,
-                  }))}
-                  search
-                  maxHeight={300}
-                  labelField="label"
-                  valueField="value"
-                  placeholder={`Location`}
-                  searchPlaceholder="Search..."
-                  value={selectedLocation} // Use selectedLocation here
-                  onChange={handleLocationChange}
-                  renderLeftIcon={() => (
-                    <AntDesign
-                      style={styles.icon}
-                      color="black"
-                      name="Safety"
-                      size={20}
-                    />
-                  )}
-                />
-              </View>
-              <View
-                style={{ marginTop: 10, marginHorizontal: 20, width: "100%" }}
-              >
-                <Dropdown
-                  style={styles.dropdown}
-                  placeholderStyle={styles.placeholderStyle}
-                  selectedTextStyle={styles.selectedTextStyle}
-                  inputSearchStyle={styles.inputSearchStyle}
-                  iconStyle={styles.iconStyle}
-                  data={monthData}
-                  maxHeight={300}
-                  labelField="label"
-                  valueField="value"
-                  placeholder="Month"
-                  value={selectedMonth} // Use selectedMonth here
-                  onChange={handleMonthChange}
-                  renderLeftIcon={() => (
-                    <AntDesign
-                      style={styles.icon}
-                      color="black"
-                      name="calendar"
-                      size={20}
-                    />
-                  )}
-                />
-              </View>
-              <View
-                style={{ marginTop: 10, marginHorizontal: 20, width: "100%" }}
-              >
-                <Dropdown
-                  style={styles.dropdown}
-                  placeholderStyle={styles.placeholderStyle}
-                  selectedTextStyle={styles.selectedTextStyle}
-                  inputSearchStyle={styles.inputSearchStyle}
-                  iconStyle={styles.iconStyle}
-                  data={yearData}
-                  maxHeight={300}
-                  labelField="label"
-                  valueField="value"
-                  placeholder="Year"
-                  value={selectedYear} // Use selectedYear here
-                  onChange={handleYearChange}
-                  renderLeftIcon={() => (
-                    <AntDesign
-                      style={styles.icon}
-                      color="black"
-                      name="calendar"
-                      size={20}
-                    />
-                  )}
-                />
-              </View>
-              <TouchableOpacity
-                style={{
-                  backgroundColor: "#21005d",
-                  padding: 10,
-                  alignItems: "center",
-                  marginTop: 10,
-                  marginBottom: 30,
-                  borderRadius: 5,
-                }}
-                onPress={searchFsgr}
-              >
-                <Text
-                  style={{ color: "white", fontSize: 14, fontWeight: "bold" }}
-                >
-                  Find FSGR
-                </Text>
-              </TouchableOpacity>
-            </View>
+            <SearchForm
+              locations={locations}
+              selectedLocation={selectedLocation}
+              setSelectedLocation={setSelectedLocation}
+              selectedMonth={selectedMonth}
+              setSelectedMonth={setSelectedMonth}
+              selectedYear={selectedYear}
+              setSelectedYear={setSelectedYear}
+              searchFsgr={searchFsgr}
+              isVisible={isVisible}
+              setIsVisible={setIsVisible}
+              setShowSearch={setShowSearch}
+            />
           )}
           {loading ? (
             <ActivityIndicator size="large" color="#0000ff" />
@@ -282,7 +143,7 @@ const FsgrInfo = () => {
                   </View>
                   <View style={styles.statusContainer}>
                     <View style={styles.statusBadge}>
-                      <Text style={styles.statusText}>Pending</Text>
+                      <Text style={styles.statusText}>{item.status}</Text>
                     </View>
                   </View>
                 </View>
@@ -297,7 +158,7 @@ const FsgrInfo = () => {
             id={id}
           />
         </View>
-      </View>
+      </ScrollView>
     </View>
   );
 };
@@ -306,37 +167,7 @@ const styles = StyleSheet.create({
   mainContainer: {
     marginTop: 20,
     height: "100%",
-    // justifyContent: "center",
     alignItems: "center",
-  },
-  searchBarContainer: {
-    flex: 1,
-    width: "100%",
-    marginBottom: 20,
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-  },
-  searchInput: {
-    width: "90%",
-    height: 42,
-    borderRadius: 8,
-    paddingStart: 10,
-    backgroundColor: "white",
-    elevation: 5,
-    marginTop: 5,
-  },
-  searchButton: {
-    width: "10%",
-    position: "absolute",
-    left: 300,
-    marginTop: 8,
-    borderLeftWidth: 0.3,
-    borderLeftColor: "grey",
-    padding: 5,
-    alignSelf: "center",
-  },
-  searchIcon: {
-    marginStart: 8,
   },
   container: {
     backgroundColor: "#fffbfe",
@@ -410,52 +241,6 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: "600",
     color: "#663c00",
-  },
-
-  // dropdown
-  dropdown: {
-    width: "90%",
-    margin: 10,
-    height: 50,
-    backgroundColor: "white",
-    borderRadius: 7,
-    padding: 12,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.2,
-    shadowRadius: 1.41,
-
-    elevation: 2,
-  },
-  icon: {
-    marginRight: 5,
-  },
-  item: {
-    padding: 17,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  textItem: {
-    flex: 1,
-    fontSize: 16,
-  },
-  placeholderStyle: {
-    fontSize: 16,
-  },
-  selectedTextStyle: {
-    fontSize: 16,
-  },
-  iconStyle: {
-    width: 20,
-    height: 20,
-  },
-  inputSearchStyle: {
-    height: 40,
-    fontSize: 16,
   },
 });
 
---
+++ screens/FsgrInfo/FsgrSearch.js
@@ -0,0 +1,189 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Modal,
+  ScrollView,
+  Dimensions,
+} from "react-native";
+import { Dropdown } from "react-native-element-dropdown";
+import { AntDesign, Entypo } from "@expo/vector-icons";
+
+const monthData = [
+  { label: "January", value: "01" },
+  { label: "February", value: "02" },
+  { label: "March", value: "03" },
+  { label: "April", value: "04" },
+  { label: "May", value: "05" },
+  { label: "June", value: "06" },
+  { label: "July", value: "07" },
+  { label: "August", value: "08" },
+  { label: "September", value: "09" },
+  { label: "October", value: "10" },
+  { label: "November", value: "11" },
+  { label: "December", value: "12" },
+];
+
+const yearData = [
+  { label: "2023", value: "2023" },
+  { label: "2024", value: "2024" },
+  { label: "2025", value: "2025" },
+  { label: "2026", value: "2026" },
+  { label: "2027", value: "2027" },
+  { label: "2028", value: "2028" },
+];
+
+const DropdownField = ({ style, placeholder, value, onChange, data, icon }) => (
+  <View style={styles.fieldContainer}>
+    <Dropdown
+      style={[styles.dropdown, style]}
+      placeholderStyle={styles.placeholderStyle}
+      selectedTextStyle={styles.selectedTextStyle}
+      inputSearchStyle={styles.inputSearchStyle}
+      iconStyle={styles.iconStyle}
+      data={data}
+      maxHeight={300}
+      labelField="label"
+      valueField="value"
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      renderLeftIcon={() => (
+        <AntDesign style={styles.icon} name={icon} size={20} />
+      )}
+    />
+  </View>
+);
+
+const FsgrSearch = ({
+  locations,
+  selectedLocation,
+  setSelectedLocation,
+  selectedMonth,
+  setSelectedMonth,
+  selectedYear,
+  setSelectedYear,
+  searchFsgr,
+  setShowSearch,
+}) => {
+  const windowHeight = Dimensions.get("window").height;
+
+  return (
+    <Modal transparent animationType="slide">
+      <View style={styles.modalOverlay}>
+        <ScrollView
+          style={[
+            styles.modalContent,
+            { height: windowHeight * 0.5, marginTop: windowHeight * 0.5 },
+          ]}
+        >
+          <View style={styles.header}>
+            <Text style={styles.headerText}>Search Your FSGR</Text>
+            <Entypo
+              name="cross"
+              size={30}
+              color="red"
+              onPress={() => setShowSearch(false)}
+            />
+          </View>
+          <DropdownField
+            placeholder="Location"
+            value={selectedLocation}
+            onChange={setSelectedLocation}
+            data={locations.map((location) => ({
+              label: location.name,
+              value: location.id,
+            }))}
+            icon="Safety"
+          />
+          <DropdownField
+            placeholder="Month"
+            value={selectedMonth}
+            onChange={setSelectedMonth}
+            data={monthData}
+            icon="calendar"
+          />
+          <DropdownField
+            placeholder="Year"
+            value={selectedYear}
+            onChange={setSelectedYear}
+            data={yearData}
+            icon="calendar"
+          />
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.searchButton} onPress={searchFsgr}>
+              <Text style={styles.searchButtonText}>Find FSGR</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalOverlay: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    backgroundColor: "#FFF",
+    width: "100%",
+    padding: 25,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
+    alignItems: "center",
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#21005d",
+  },
+  fieldContainer: {
+    marginTop: 10,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  dropdown: {
+    width: "100%",
+    height: 50,
+    backgroundColor: "white",
+    borderRadius: 7,
+    padding: 12,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
+    marginVertical: 5,
+  },
+  icon: { marginRight: 5 },
+  placeholderStyle: { fontSize: 16 },
+  selectedTextStyle: { fontSize: 16 },
+  iconStyle: { width: 20, height: 20 },
+  inputSearchStyle: { height: 40, fontSize: 16 },
+  buttonContainer: { justifyContent: "center", alignItems: "center" },
+  searchButton: {
+    backgroundColor: "#21005d",
+    width: "100%",
+    padding: 12,
+    alignItems: "center",
+    marginTop: 10,
+    marginBottom: 30,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  searchButtonText: { color: "white", fontSize: 14, fontWeight: "bold" },
+});
+
+export default FsgrSearch;
