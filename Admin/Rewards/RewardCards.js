5b3e11c0114f1148939f8aa06b8b851cbd51e5bc
Fahad Mahmood <fahadmahmood@Fahads-MacBook-Air.local>
2024-06-22 03:27:00 +0530

    small fixes

--- Admin/Rewards/RewardCards.js
+++ Admin/Rewards/RewardCards.js
@@ -6,40 +6,78 @@ const RewardCards = ({ rewards }) => {
   return (
     <View style={styles.rewardsContainer}>
       {rewards?.length === 0 ? (
-        <View
-          style={{
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Image
-            source={noData}
-            style={{
-              width: "100%",
-              height: 400,
-            }}
-          />
-          <Text
-            style={{
-              fontSize: 16,
-              fontWeight: "600",
-              color: "gray",
-            }}
-          >
-            Please Search the Data
-          </Text>
+        <View style={styles.noDataContainer}>
+          <Image source={noData} style={styles.noDataImage} />
+          <Text style={styles.noDataText}>Please Search the Data</Text>
         </View>
       ) : (
         rewards.map((reward, index) => (
           <View key={index} style={styles.card}>
-            <Text style={styles.cardText}>Employee Name: {reward.empName}</Text>
-            <Text style={styles.cardText}>Employee ID: {reward.empId}</Text>
-            <Text style={styles.cardText}>Location: {reward.location}</Text>
-            <Text style={styles.cardText}>
-              Short Description: {reward.shortDiscription}
-            </Text>
-            <Text style={styles.cardText}>Reward: {reward.Reward}</Text>
-            <Text style={styles.cardText}>Price: {reward.Price}</Text>
+            <View style={styles.cardRow}>
+              <View style={styles.cardColumn}>
+                <Text
+                  style={{
+                    fontSize: 16,
+                    fontWeight: "600",
+                    color: "#21005d",
+                  }}
+                >
+                  {reward.empName}
+                </Text>
+                <Text
+                  style={{
+                    marginVertical: 4,
+                    fontSize: 14,
+                    fontWeight: "300",
+                    color: "#21005d",
+                  }}
+                >
+                  {reward.shortDiscription}
+                </Text>
+                <Text
+                  style={{
+                    marginVertical: 4,
+                    fontSize: 14,
+                    fontWeight: "300",
+                    color: "#21005d",
+                  }}
+                >
+                  {reward.Reward}
+                </Text>
+                <Text
+                  style={{
+                    marginVertical: 4,
+                    fontSize: 16,
+                    fontWeight: "600",
+                    color: "#21005d",
+                  }}
+                >
+                  Price: {reward.Price}
+                </Text>
+              </View>
+              <View style={styles.cardColumn}>
+                <Text
+                  style={{
+                    marginVertical: 4,
+                    fontSize: 18,
+                    fontWeight: "600",
+                    color: "#21005d",
+                  }}
+                >
+                  {reward.location}
+                </Text>
+                <Text
+                  style={{
+                    marginVertical: 4,
+                    fontSize: 14,
+                    fontWeight: "400",
+                    color: "#21005d",
+                  }}
+                >
+                  {reward.createdAt.slice(0, 10)}
+                </Text>
+              </View>
+            </View>
           </View>
         ))
       )}
@@ -52,16 +90,38 @@ const styles = StyleSheet.create({
     marginTop: 20,
     padding: 20,
   },
+  noDataContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  noDataImage: {
+    width: "100%",
+    height: 400,
+  },
+  noDataText: {
+    fontSize: 16,
+    fontWeight: "600",
+    color: "gray",
+  },
   card: {
     padding: 15,
-    borderWidth: 1,
-    borderColor: "#ddd",
+    backgroundColor: "#fffbfe",
+    elevation: 5,
     borderRadius: 5,
     marginBottom: 10,
+  },
+  cardRow: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  cardColumn: {
+    marginRight: 10,
   },
   cardText: {
     fontSize: 16,
     marginBottom: 5,
+    color: "#505050",
   },
   noRewardsText: {
     fontSize: 16,
