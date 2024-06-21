2a7f36536f2e7884cab65c9d6c517478522175e0
Fahad Mahmood <fahadmahmood@Fahads-MacBook-Air.local>
2024-06-21 19:58:59 +0530

    mass meating

---
+++ SafetyManager/Screens/MassMeating/MassMeating.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const MassMeating = () => {
+  return (
+    <View>
+      <Text>MassMeating</Text>
+    </View>
+  );
+};
+
+export default MassMeating;
