c832787cf4237b073eefc8c8b874133ffb4808a7
Fahad Mahmood <fahadmahmood@Fahads-MacBook-Air.local>
2024-06-22 12:20:34 +0530

    under cons

--- SafetyManager/Screens/SafetyManagerHome/Training/Training.js
+++ SafetyManager/Screens/SafetyManagerHome/Training/Training.js
@@ -1,10 +1,27 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
+import UnderConstruction from "../../../../components/Global/UnderConstruction/UnderConstruction";
 
 const Training = () => {
   return (
-    <View>
-      <Text>Training</Text>
+    <View
+      style={{
+        height: "100%",
+        backgroundColor: "white",
+        alignItems: "center",
+        flex: 1,
+        justifyContent: "center",
+      }}
+    >
+      <Image
+        source={{
+          uri: "https://media.istockphoto.com/id/1411798446/vector/modern-coming-soon-under-construction-sticker-banner.jpg?s=612x612&w=0&k=20&c=KgQ9eYEfuzTdmc5ypmMYoz8JHPTbe_TjEXxyxNixKQ4=",
+        }}
+        style={{
+          height: "40%",
+          width: "100%",
+        }}
+      />
     </View>
   );
 };
