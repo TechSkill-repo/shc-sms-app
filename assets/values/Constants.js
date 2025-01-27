8361eca6fdaf9f53665f030a50df7d3c274ee1de
Fahad Mahmood <fahadmahmood@Fahads-MacBook-Air.local>
2025-01-27 17:06:05 +0530

    new url

--- assets/values/Constants.js
+++ assets/values/Constants.js
@@ -4,7 +4,10 @@ import { Dimensions } from "react-native";
 
 // const serveraddress = "http://192.168.196.143:8080/";
 
-const serveraddress = "https://sms-be-fj4h.onrender.com/";
+// const serveraddress = "https://sms-be-fj4h.onrender.com/";
+
+const serveraddress =
+  "https://shc-sms-d6bdf7e3gtgpcgdz.ukwest-01.azurewebsites.net/";
 
 const width = Dimensions.get("window").width;
 const height = Dimensions.get("window").height;
